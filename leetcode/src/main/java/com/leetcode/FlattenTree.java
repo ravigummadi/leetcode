d40d3ed41006a3e2c6dd0e768b3991f1f35c5c55
Ravi Gummadi <gummadi@asu.edu>
2013-01-30 23:08:41 -0800

    Flattening Binray Tree

---
+++ leetcode/src/main/java/com/leetcode/FlattenTree.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ravigummadi
+ * Date: 1/30/13
+ * Time: 8:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FlattenTree {
+
+    public void flatten(TreeNode root) {
+        recursiveFlatten(root);
+    }
+
+
+    private TreeNode recursiveFlatten(TreeNode current){
+        if(current == null) return null;
+        if(current.left != null){
+            TreeNode nextRight = current.right;
+            current.right = current.left;
+            current.left = null;
+            TreeNode nextNode = recursiveFlatten(current.right);
+            nextNode.right = nextRight;
+            return  recursiveFlatten(nextNode);
+        }else if(current.right != null){
+            return recursiveFlatten(current.right);
+        }else{
+            return current;
+        }
+    }
+}
