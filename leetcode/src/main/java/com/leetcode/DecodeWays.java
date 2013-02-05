b82aa144f891be5b905b1a3e7083029544436dfb
Ravi Gummadi <gummadi@asu.edu>
2013-02-04 21:13:01 -0800

    Recursive version of DecodeWays

---
+++ leetcode/src/main/java/com/leetcode/DecodeWays.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ravigummadi
+ * Date: 1/31/13
+ * Time: 10:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DecodeWays {
+
+    int count = 0;
+
+    public int numDecodings(String s) {
+        count = 0;
+        if(s != null && s.length()>0)
+            recNumDecodings(s);
+        return  count;
+    }
+
+    private void recNumDecodings(String s){
+        if(s == null || s.length() == 0 ){
+            count++;
+            return;
+        }
+
+        if(s.length() > 0){
+            if(s.charAt(0) == '0')
+                return;
+            else{
+                recNumDecodings(s.substring(1));
+            }
+        }
+
+        if(s.length() > 1){
+            int twoDigitNum = Integer.parseInt(s.substring(0,2));
+            if(twoDigitNum < 10 || twoDigitNum > 26){
+                return;
+            }else{
+                recNumDecodings(s.substring(2));
+            }
+        }
+
+    }
+
+    private void recNumDecoding(String s){
+        
+    }
+
+}
+
---
+++ leetcode/src/main/java/com/leetcode/ValidBST.java
@@ -0,0 +1,11 @@
+package com.leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ravigummadi
+ * Date: 1/30/13
+ * Time: 11:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ValidBST {
+}
